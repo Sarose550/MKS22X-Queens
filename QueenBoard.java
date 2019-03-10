585cd4b7d93e7da7c0b3b6737ccaab6770ccbe49
Sarose550 <43526712+Sarose550@users.noreply.github.com>
2019-03-10 16:09:09 -0400

    Update QueenBoard.java

--- QueenBoard.java
+++ QueenBoard.java
@@ -1,99 +1,94 @@
-public class KnightBoard{
-
+import java.io.IOException;
+public class QueenBoard{
 	private int[][] board;
-	private int startingRows;
-	private int startingCols;
-	//@throws IllegalArgumentException when either parameter is negative.
-public KnightBoard(int startingRows,int startingCols){
-	this.startingRows = startingRows;
-	this.startingCols = startingCols;
-	board = new int[startingRows][startingCols];
-}
- 
-    //Initialize the board to the correct size and make them all 0's 
 
+	public QueenBoard(int size){
+		board = new int[size][size];
+	}
 
-public String toString(){
-	String result = "";
-	for(int i = 0; i < startingRows; i++){
-		for(int j = 0; j < startingCols; j++){
-			if(startingRows * startingCols >= 10 && board[i][j] < 10){
-				result += "  ";
-			}
-			else{
+	private boolean addQueen(int r, int c){
+		board[r][c] = 1;
+		return true;
+	}
+
+	private boolean removeQueen(int r, int c){
+		board[r][c] = 0;
+		return true;
+	}
+
+	public String toString(){
+		String result = "";
+		for(int i = 0; i < board.length; i++){
+			for(int j = 0; j < board.length; j++){
+				if(board[i][j] == 0) result += "_";
+				else{
+					result += "Q";
+				}
 				result += " ";
 			}
-			if(board[i][j] == 0){
-				result += "_";
-			}
-			else{
-				result += Integer.toString(board[i][j]);
-			}
+			result += "\n";
 		}
-		result += "\n";
+		return result;
 	}
-	return result;
-} 
-/*see format for toString below
-blank boards display 0's as underscores 
-you get a blank board if you never called solve or 
-when there is no solution 
-*/
-/*@throws IllegalStateException when the board contains non-zero values.
-@throws IllegalArgumentException when either parameter is negative 
- or out of bounds.
- */
-public boolean solve(int startingRow, int startingCol){
-	for(int i = startingRow; i < startingRows; i++){
-		for(int j = startingCol; j < startingCols; j++){
-			if(board[i][j] != 0) throw new IllegalStateException("Board contains non-zero values.");
-			if(startingRow < 0 || startingRow >= startingRows || startingCol < 0 || startingCol >= startingCols){
-				throw new IllegalArgumentException("Parameter out of bounds.");
-			}
-		}
-	}
-	board[startingRow][startingCol] = 1;
-	if(this.solveH(startingRow, startingCol, 1)) return true;
-	board[startingRow][startingCol] = 0;
-	return false;
-}
 
-/*@throws IllegalStateException when the board contains non-zero values. 
-@throws IllegalArgumentException when either parameter is negative 
- or out of bounds.*/
-//public int countSolutions(int startingRow, int startingCol)
-
-//Suggestion:
-private boolean solveH(int row, int col, int level){
-	for(int i = -2; i <= 2; i += 4){
-		for(int j = -1; j <= 1; j += 2){
-			if(row + i >= 0 && row + i < startingRows){
-				if(col+ j >= 0 && col + j < startingCols){
-					if(board[row + i][col + j] == 0){
-						board[row + i][col + j] = level + 1;
-						if(level == startingRows * startingCols - 1) return true;
-						if(this.solveH(row + i, col + j, level + 1)){
-							return true;
-						}
-						board[row + i][col + j] = 0;
-					}
-				}
-			}
-			if(row + j >= 0 && row + j < startingRows){
-				if(col + i >= 0 && col + i < startingCols){
-					if(board[row + j][col + i] == 0){
-						board[row + j][col + i] = level + 1;
-						if(level == startingRows * startingCols - 1) return true;
-						if(this.solveH(row + j, col + i, level + 1)){
-							return true;
-						}
-						board[row + j][col + i] = 0;
-					}
+	public boolean solve(){
+		for(int i = 0; i < board.length; i++){
+			for(int j = 0; j < board.length; j++){
+				if(board[i][j] != 0){
+					throw new IllegalStateException("Board started with a nonzero value.");
 				}
 			}
 		}
+		return this.oneSolution(0);
 	}
-	return false;
-}
-// level is the # of the knight
+
+	public boolean oneSolution(int r){
+		for(int i = 0; i < board.length; i++){
+			if(!isConflict(board, r, i)){//only place if there's no conflict
+				this.addQueen(r, i);
+				if(r == board.length - 1) return true; //you're done
+				if(oneSolution(r + 1)) return true; //if going forward yeilds a solution, return true
+				this.removeQueen(r, i);//if you didn't get a solution, remove the queen and keep going
+			}
+		}
+		return false;
+	}
+
+	public boolean isConflict(int[][] board, int r, int c){
+		for(int i = 0; i < board.length; i++){
+			for(int j = 0; j < board.length; j++){
+				if(board[i][j] == 1){
+					if(Math.abs(r - i) == Math.abs(c - j)) return true;
+					if(j == c) return true;
+				}
+			}
+		}
+		return false;
+	}
+
+	public int countSolutions(){
+		for(int i = 0; i < board.length; i++){
+			for(int j = 0; j < board.length; j++){
+				if(board[i][j] != 0){
+					throw new IllegalStateException("Board started with a nonzero value.");
+				}
+			}
+		}
+		return this.allSolutions(0);
+	}
+
+	public int allSolutions(int r){
+		int total = 0;
+		for(int i = 0; i < board.length; i++){
+			if(!isConflict(board, r, i)){//only place if there's no conflict
+				this.addQueen(r, i);
+				if(r == board.length - 1) total += 1; //you've got one
+				else{
+					total += allSolutions(r + 1); //increase the depth of the search and add the number of solutions found
+				}
+				this.removeQueen(r, i);//remove the queen, keep searching
+			}
+		}
+		return total;
+	}
 }
